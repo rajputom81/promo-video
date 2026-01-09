f75337b850774bb270900f6e0488bdeabb2856c9
Om Rajput <rajputom0101@gmail.com>
2026-01-09 17:56:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const videos = [
+  "source/videos/v1.mp4",
+  "source/videos/v2.mp4",
+  "source/videos/v3.mp4",
+  "source/videos/v4.mp4"
+];
+
+let index = 0;
+const videoPlayer = document.getElementById("videoPlayer");
+
+function playNextVideo() {
+  videoPlayer.src = videos[index];
+  videoPlayer.play();
+}
+
+videoPlayer.addEventListener("ended", () => {
+  index = (index + 1) % videos.length; // loop playlist
+  playNextVideo();
+});
+
+// start first video
+playNextVideo();
