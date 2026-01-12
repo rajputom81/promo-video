270df7e1ee6c1f7f8c0d27b2dbf1b5a4f3aebe1f
Om Rajput <rajputom0101@gmail.com>
2026-01-12 17:55:56 +0530

    update js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const videos = [
-  "source/videos/v1.mp4",
-  "source/videos/v2.mp4",
-  "source/videos/v3.mp4",
+  "source/video/v1.mp4",
+  "source/video/v2.mp4",
+  "source/video/v3.mp4",
 ];
 
 let index = 0;
